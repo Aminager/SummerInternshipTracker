fef9f2cfa71a964fc1224059918b32ad4bd469c0
Amin Alian <aminagerlol@gmail.com>
2021-11-22 14:04:00 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
-import AddItem from './components/AddItem';
 import CreatedBy from './components/CreatedBy';
+import Form from './components/Form'
+import List from './components/List'
 
 export default function App() {
+  const [companies, setCompanies] = useState([{
+      company: 'IKEA',
+      city: 'Stockholm',
+      role: 'Software Engineer'
+    },
+    {
+      company: 'Axis',
+      city: 'Lund',
+      role: 'Software Engineer'
+    }
+  ])
+
+  const [cName, setCName] = useState("")
+  const [cCity, setCCity] = useState("")
+  const [cRole, setCRole] = useState("")
+
   return (
     <div className="App">
-      <Header header="Swedish SE internships"/>
-      <AddItem />
-      <CreatedBy name1="Amin Alian" name2="Ludvig Gierup"/>
+      <Header header="Swedish SE internships" />
+      <Form
+        companies={companies}
+        setCompanies={setCompanies}
+        cName={cName}
+        setCName={setCName}
+        cCity={cCity}
+        setCCity={setCCity}
+        cRole={cRole}
+        setCRole={setCRole}
+      />
+      <List companies={companies}/>
+      <CreatedBy name1="Amin Alian" name2="Ludvig Gierup" />
     </div>
   );
 }
